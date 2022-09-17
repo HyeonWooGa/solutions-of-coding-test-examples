e48964fdee6eb302b1fa36015ca387a1b8a35e0e
박연우 <yeonwoopark22@gmail.com>
2022-09-18 03:09:51 +0900

    [level 1] Title: 음양 더하기, Time: 3.92 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,8 @@
+function solution(absolutes, signs) {
+    const arr = [];
+    signs.forEach((sign, i) => sign === true ? arr[i] = absolutes[i] : arr[i] = -absolutes[i]);
+    
+    console.log(arr);
+    
+    return arr.reduce((acc, cur) => acc + cur, 0);
+}
