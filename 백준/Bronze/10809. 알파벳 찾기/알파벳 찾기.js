c548ef2e7a732d38080491a8f2ee899475d65591
박연우 <yeonwoopark22@gmail.com>
2022-09-25 01:38:52 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+const wordArr = input.split('');
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const result = alphabet.map(el => wordArr.indexOf(el));
+
+let string = '';
+
+result.forEach((el) => {
+  string = string + `${el} `
+})
+
+console.log(string);
