66dd5384db15ac3d77b047e5a530574a8accac18
박연우 <yeonwoopark22@gmail.com>
2022-09-25 23:39:40 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1384 ms, Memory: 220292 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const [n, ...numbers] = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+let ans = "";
+
+numbers.sort((a, b) => a - b).forEach((num) => ans += `${num}\n`);
+
+console.log(ans);
