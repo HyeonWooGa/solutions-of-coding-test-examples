9f2568eb56649830f66635894659c228ce8caef9
박연우 <yeonwoopark22@gmail.com>
2022-10-04 23:32:53 +0900

    [level 1] Title: 최소직사각형, Time: 8.19 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,3 +1,25 @@
+// 개선 후
+// 의사 코드
+// 1. 사이즈들을 탐색하며 각각의 명함 사이즈 배열을 오름차순으로 정렬 합니다.
+// 2. 명함 사이즈의 큰값끼리, 작은 값끼리 비교해서 각각 큰값, 작은값 중에서의 최댓값을 구합니다.
+// 3. 2. 에서 구해진 큰 값중에 최댓값과 작은 값중에 최댓값을 곱해줍니다.
+
+function solution(sizes) {
+    let min = 0;
+    let max = 0;
+    
+    sizes.forEach((size) => {
+        const [x, y] = size.sort((x, y) => x - y);
+        min = x > min ? x : min;
+        max = y > max ? y : max;
+    })
+    
+    return min * max;
+}
+
+
+// 개선 전
+/*
 // 의사 코드
 // 1. 모든 요소의 숫자 중에서 가장 큰 값을 찾고 해당 명함의 가로 세로 길이를 저장합니다.
 // 2. 해당 명함을 인자 배열에서 제거합니다.
@@ -29,3 +51,4 @@ function solution(sizes) {
     
     return max * elseNum;
 }
+*/
