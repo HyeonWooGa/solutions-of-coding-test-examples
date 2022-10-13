8fde2f48a0f6290b2361a83cfa24d9f054a8b589
박연우 <yeonwoopark22@gmail.com>
2022-10-13 11:36:04 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -4,13 +4,13 @@ function solution(operations) {
     
     operations.forEach((operation) => {
         arr.sort((a, b) => a - b);
-        const num = +operation[1];
+        
         switch(operation[0]) {
             case "I":
-                arr.push(num);
+                arr.push(+operation[1]);
                 break;
             case "D":
-                if (num === 1) {
+                if (operation[1] === "1") {
                     arr.pop();
                 }
                 else {
@@ -19,7 +19,6 @@ function solution(operations) {
                 break;
         }
     })
-    console.log(arr);
     
     return arr.length === 0 ? [0, 0] : [Math.max(...arr), Math.min(...arr)];
 }
