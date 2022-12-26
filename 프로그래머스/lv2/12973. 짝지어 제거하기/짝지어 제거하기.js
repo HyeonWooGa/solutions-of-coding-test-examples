a95ab0ef2be5920aecd7f558aa6fe7cc446f9bd4
박연우 <yeonwoopark22@gmail.com>
2022-12-26 23:19:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 41.93 ms, Memory: 56.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,24 @@
+function solution(s)
+{
+    let index = 0;
+    let slicedStr = s.split('');
+    let result = [];
+    
+    while(index < slicedStr.length) {
+        if (slicedStr[index] === slicedStr[index+1] && slicedStr.length !== 0) {
+            index += 2;
+        } 
+        else {
+            if (result[result.length - 1] !== slicedStr[index])
+                {
+                    result.push(slicedStr[index]);
+                }
+            else {
+                result.pop();
+            }
+            index += 1;
+        }
+    }
+    
+    return result.length === 0 ? 1 : 0
+}
