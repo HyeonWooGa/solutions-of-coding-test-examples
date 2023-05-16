dcfd91c723ad9af30e448c45fb8c5fcf827f99b1
박연우 <yeonwoopark22@gmail.com>
2023-05-16 22:37:49 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,14 @@
+function solution(citations) {
+    let answer = 0;
+    
+    citations.sort((a,b)=>{
+        return b-a;
+    });
+    var n=citations.length;
+    for(var i=0 ; i<n; ++i) {
+        if(i+1<=citations[i]) {
+            answer++;
+        }
+    }
+    return answer;
+}
