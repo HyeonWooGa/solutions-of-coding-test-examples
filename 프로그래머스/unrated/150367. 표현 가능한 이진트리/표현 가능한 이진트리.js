bd4fe15f40cb19add9d112e0dd7405b689c6abbb
박연우 <yeonwoopark22@gmail.com>
2023-10-05 22:44:37 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 22.34 ms, Memory: 42.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
@@ -0,0 +1,26 @@
+function solution(numbers) {
+  numbers = numbers.map((number) => number.toString(2).split('').reverse());
+
+  const isPossible = (number) => {
+    while (true) {
+      if (number.length === 1 && number[0] === '1') return true;
+      const newNumber = [];
+
+      for (let i = 0; i < number.length; i += 4) {
+        if (number[i + 1] === '0' || !number[i + 1]) {
+          if (number[i] === '1' || number[i + 2] === '1') return false;
+        }
+
+        newNumber.push(number[i + 1] || '0');
+
+        if (number[i + 3]) newNumber.push(number[i + 3]);
+      }
+
+      number = newNumber;
+    }
+  };
+
+  const result = numbers.map((number) => (isPossible(number) ? 1 : 0));
+
+  return result;
+}
