99455928931f44fec04df8a2e5f25276245e41f4
박연우 <yeonwoopark22@gmail.com>
2023-10-06 21:39:18 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,8 +1,7 @@
 function solution(n, k) {
     let total = 0;
     
-    total += (n * 12000);
-    total += ((k - Math.floor(n / 10)) * 2000);
+    let shouldPayBaverage = k - (Math.floor(n / 10));
     
-    return total;
+    return (n * 12000) + (shouldPayBaverage * 2000);
 }
